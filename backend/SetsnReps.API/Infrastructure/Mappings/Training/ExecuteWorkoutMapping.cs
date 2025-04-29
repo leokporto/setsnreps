a48dc165ee38d9e7479c6d9a636b580b6755083a
Leonardo Porto <leokporto@gmail.com>
2025-04-29 08:11:40 -0300

    Added ExecutedWorkoutMapping

---
+++ backend/SetsnReps.API/Infrastructure/Mappings/Training/ExecuteWorkoutMapping.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SetsnReps.Core.Models.Training;
+
+namespace SetsnReps.API.Infrastructure.Mappings.Training;
+
+public class ExecutedWorkoutMapping : IEntityTypeConfiguration<ExecutedWorkout>
+{
+   
+    public void Configure(EntityTypeBuilder<ExecutedWorkout> builder)
+    {
+        builder.ToTable("ExecutedWorkouts");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.WorkoutRoutineId).IsRequired();
+        builder.Property(x => x.Date).IsRequired();
+        builder.Property(x => x.Duration).IsRequired();
+        builder.Property(x => x.WeightVolume).IsRequired();
+
+        builder.HasOne(x => x.WorkoutRoutine)
+            .WithMany()
+            .HasForeignKey(x => x.WorkoutRoutineId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(x => x.WorkoutRoutineId);
+    }
+    
+}
